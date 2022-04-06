93f7c1e3b748c7814e2169e4bba544e380194f6b
lmeinen <lmeinen@student.ethz.ch>
2022-04-06 14:59:44 +0200

    added prettier color cycle and markers to performance plots

--- measurements/plot.py
+++ measurements/plot.py
@@ -1,11 +1,15 @@
 import argparse
-from cProfile import label
 import csv
 import os
+from cProfile import label
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser()
+COLOR_LIST = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a',
+              '#66a61e', '#e6ab02', '#a6761d', '#666666']
+
+parser = argparse.ArgumentParser(description="generate performance plot using data from a csv file")
 parser.add_argument("-d", "--data", help="csv file with data", type=str, required=True)
 parser.add_argument(
     "-p", "--plot", help="directory to save the plots", type=str, required=True
@@ -25,8 +29,10 @@ with open(data_file) as f:
     runs_list = reader.__next__()
     cycles_list = reader.__next__()
 
-plt.plot(n_list, runs_list, "-o", label="runs")
-plt.plot(n_list, cycles_list, "-o", label="cycles")
+mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=COLOR_LIST) # sets colors using given cycle
+
+plt.plot(n_list, runs_list, "-o", label="runs", marker='^')
+plt.plot(n_list, cycles_list, "-o", label="cycles", marker='^')
 
 plt.xticks(n_list)
 
@@ -37,11 +43,16 @@ plt.ylim([0, max(2, ymax + 0.1 * ymax)])
 
 plt.grid(True, which="major", axis="y")
 
-plt.legend()
+plt.legend() # TODO: Label lines directly
 
 plt.title(title)
 plt.xlabel("n")
+# plt.semilogx(base=2)
 plt.ylabel("cycles")
 
-data_file_name = os.path.basename(data_file)
-plt.savefig("{}/{}.png".format(plots_dir, data_file_name))
+data_file_name = os.path.basename(data_file).replace(".csv", "")
+outfile = "{}/{}.png".format(plots_dir, data_file_name)
+plt.savefig(outfile)
+plt.clf()
+
+print("stored performance plot to {}".format(outfile))
