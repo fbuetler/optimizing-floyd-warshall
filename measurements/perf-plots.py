c65493f97996a0ee01a4990ce8de621fff95016b
Florian Buetler <flbuetle@ethz.ch>
2022-04-06 15:38:28 +0200

    python format

--- measurements/perf-plots.py
+++ measurements/perf-plots.py
@@ -6,10 +6,20 @@ from cProfile import label
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-COLOR_LIST = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a',
-              '#66a61e', '#e6ab02', '#a6761d', '#666666']
+COLOR_LIST = [
+    "#1b9e77",
+    "#d95f02",
+    "#7570b3",
+    "#e7298a",
+    "#66a61e",
+    "#e6ab02",
+    "#a6761d",
+    "#666666",
+]
 
-parser = argparse.ArgumentParser(description="generate performance plot using data from a csv file")
+parser = argparse.ArgumentParser(
+    description="generate performance plot using data from a csv file"
+)
 parser.add_argument("-d", "--data", help="csv file with data", type=str, required=True)
 parser.add_argument(
     "-p", "--plot", help="directory to save the plots", type=str, required=True
@@ -29,9 +39,11 @@ with open(data_file) as f:
     runs_list = reader.__next__()
     cycles_list = reader.__next__()
 
-mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=COLOR_LIST) # sets colors using given cycle
+mpl.rcParams["axes.prop_cycle"] = mpl.cycler(
+    color=COLOR_LIST
+)  # sets colors using given cycle
 
-plt.plot(n_list, cycles_list, "-o", label="cycles", marker='^')
+plt.plot(n_list, cycles_list, "-o", label="cycles", marker="^")
 
 plt.xticks(n_list)
 
@@ -42,7 +54,7 @@ plt.ylim([0, max(2, ymax + 0.1 * ymax)])
 
 plt.grid(True, which="major", axis="y")
 
-plt.legend() # TODO: Label lines directly
+plt.legend()  # TODO: Label lines directly
 
 plt.title(title)
 plt.xlabel("n")
