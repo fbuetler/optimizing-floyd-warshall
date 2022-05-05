e27322f8d06922e2270024ed041127e7d4004386
lmeinen <lmeinen@student.ethz.ch>
2022-05-05 10:33:15 +0200

    make performance plot lin-log & remove unnecessary runtime plot stuff

--- measurements/perf-plots.py
+++ measurements/perf-plots.py
@@ -42,37 +42,11 @@ with open(data_file) as f:
 # compute performance (flops = n^3)
 perf_list = list()
 for (n, c, r) in zip(n_list, cycles_list, runs_list):
-    perf_list.append((n * n * n) / c)
+    perf_list.append((2 * n * n * n) / c)
 
 mpl.rcParams["axes.prop_cycle"] = mpl.cycler(
     color=COLOR_LIST
 )  # sets colors using given cycle
-
-plt.plot(n_list, cycles_list, label="cycles", marker="^")
-
-plt.xticks(n_list)
-
-runs_max = max(runs_list)
-cycles_max = max(cycles_list)
-ymax = max(runs_max, cycles_max)
-plt.ylim([0, max(2, ymax + 0.1 * ymax)])
-
-plt.grid(True, which="major", axis="y")
-
-plt.legend()  # TODO: Label lines directly
-
-plt.title(title)
-plt.xlabel("n")
-# plt.semilogx(base=2)
-plt.ylabel("cycles")
-
-data_file_name = os.path.basename(data_file).replace(".csv", "")
-outfile = "{}/{}_cycles.png".format(plots_dir, data_file_name)
-plt.savefig(outfile)
-plt.clf()
-
-print("stored cycle plot to {}".format(outfile))
-
 
 plt.plot(n_list, perf_list, label="performance", marker="^")
 
@@ -88,8 +62,10 @@ plt.legend()
 
 plt.title(title)
 plt.xlabel("n")
-plt.ylabel("cycles")
+plt.semilogx(base=2)
+plt.ylabel("[flops/cycle]")
 
+data_file_name = os.path.basename(data_file).replace(".csv", "")
 outfile = "{}/{}_perf.png".format(plots_dir, data_file_name)
 plt.savefig(outfile)
 plt.clf()
