7a8d166c5fd5e0bbb11ceb932a38c2f2bc0381a1
nigglir <nigglir@student.ethz.ch>
2022-05-13 15:21:58 +0200

    better comment

--- transitive-closure/c/impl/naive.c
+++ transitive-closure/c/impl/naive.c
@@ -4,7 +4,9 @@
 /* unoptimized version of the modified Floyd-Warshall transitive-closure algorithm
  * the provided matrix is in packed-boolean form, meaning value [x, y] is only one bit long
  * @param N is the number of vertices
- * - 1 line of the matrix is CEIL(N / 8) bytes (or chars) long to make it more bearable to wor with
+ * - 1 line of the matrix is CEIL(N / 8) bytes (or chars) long to make it more bearable to work with,
+ *   so if n % 8 != 0, there will be some unused bits at the end of each line, but in total, at most
+ *   N * (7 / 8) bytes will be wasted.
  */
 int floydWarshall(char *C, int N) {
   int bpl = ceil(N / 8.0); // bytes per matrix line
@@ -14,7 +16,7 @@ int floydWarshall(char *C, int N) {
       char cikb = C[i * bpl + k / 8];
       int b_index = k % 8;
       char mask = 1 << b_index;
-      char cik = cikb & mask ? 0xff : 0x00; // if this is 0, we can skip everything
+      char cik = cikb & mask ? 0xff : 0x00; // if this is 0, we can skip the innermost loop
       for (int j = 0; j < bpl; j++) {
         C[i * bpl + j] = C[i * bpl + j] | (cik & C[k * bpl + j]);
       }
