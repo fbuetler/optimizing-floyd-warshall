0f8729bdb168051e5b0ffcfe28d61f793f4254ad
nigglir <nigglir@student.ethz.ch>
2022-05-14 12:50:14 +0200

    use partial vector for remaining operations

--- shortest-path/c/impl/vector.c
+++ shortest-path/c/impl/vector.c
@@ -21,9 +21,23 @@ int floydWarshall(float *C, int N) {
         // store
         _mm256_storeu_ps(&C[i * N + j], res);
       }
-      for (; j < N; j++) {
-        C[i * N + j] = MIN(C[i * N + j], C[i * N + k] + C[k * N + j]);
-      }
+
+      // only use part of a vector for the last remaining operations
+      // compute mask
+      __m256i cand_j = _mm256_set_epi32(j + 7, j + 6, j + 5, j + 4, j + 3, j + 2, j + 1, j);
+      __m256i cmp = _mm256_set1_epi32(N);
+      __m256i mask = _mm256_cmpgt_epi32(cmp, cand_j);
+
+      // load
+      __m256 cij = _mm256_maskload_ps(&C[i * N + j], mask);
+      __m256 ckj = _mm256_maskload_ps(&C[k * N + j], mask);
+
+      // compute
+      __m256 sum = _mm256_add_ps(cik, ckj);
+      __m256 res = _mm256_min_ps(cij, sum);
+
+      // store
+      _mm256_maskstore_ps(&C[i * N + j], mask, res);
     }
   }
   return 0;
