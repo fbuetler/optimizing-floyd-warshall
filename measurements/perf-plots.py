3e1f7acf9e8423ff0614868910fd4cc90c0762fa
Florian Buetler <flbuetle@ethz.ch>
2022-05-19 14:54:29 +0200

    proper script setup

--- measurements/perf-plots.py
+++ measurements/perf-plots.py
@@ -42,69 +42,81 @@ parser.add_argument(
     default=0.0,
 )
 
-args = parser.parse_args()
 
-data_file_list = args.data
-label_list = args.labels
-plots_dir = args.plot
-title = args.title
-peak = args.peak
+def main(
+    data_file_list,
+    label_list,
+    plots_dir,
+    title,
+    peak,
+):
 
-if len(data_file_list) != len(label_list):
-    raise Exception(
-        "List of labels must have the same length as the list of data file names: data had len {}, labels {}".format(
-            len(data_file_list), len(label_list)
+    if len(data_file_list) != len(label_list):
+        raise Exception(
+            "List of labels must have the same length as the list of data file names: data had len {}, labels {}".format(
+                len(data_file_list), len(label_list)
+            )
         )
+
+    data_label_list = zip(data_file_list, label_list)
+
+    if peak != 0.0:
+        _, ax = plt.subplots()
+        plt.axhline(
+            y=peak,
+            label="P ≤ π",
+            linewidth=1,
+            color=next(ax._get_lines.prop_cycler)["color"],
+        )
+
+    mpl.rcParams["axes.prop_cycle"] = mpl.cycler(
+        color=COLOR_LIST
+    )  # sets colors using given cycle
+
+    perf_max = 0.0
+
+    for data_file, label in data_label_list:
+        performance_data = list()
+        with open(data_file) as f:
+            reader = csv.reader(f, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
+            n_list = reader.__next__()
+            runs_list = reader.__next__()
+            cycles_list = reader.__next__()
+
+        # compute performance (flops = n^3)
+        perf_list = list()
+        for (n, c, r) in zip(n_list, cycles_list, runs_list):
+            perf_list.append((2 * n * n * n) / c)
+
+        plt.plot(n_list, perf_list, label=label, marker="^")
+
+        perf_max = max(perf_list + [perf_max])
+
+    plt.ylim(0, max(perf_max + 0.1 * perf_max, peak + 0.1 * peak))
+
+    plt.grid(True, which="major", axis="y")
+
+    plt.legend()
+
+    plt.title(title)
+    plt.xlabel("n")
+    plt.semilogx(base=2)
+    plt.ylabel("[flops/cycle]")
+
+    data_file_name = os.path.basename(data_file).replace(".csv", "")
+    outfile = "{}/{}_perf.png".format(plots_dir, data_file_name)
+    plt.savefig(outfile)
+    plt.clf()
+
+    print("stored performance plot to {}".format(outfile))
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(
+        args.data,
+        args.labels,
+        args.plot,
+        args.title,
+        args.peak,
     )
-
-data_label_list = zip(data_file_list, label_list)
-
-if peak != 0.0:
-    _, ax = plt.subplots()
-    plt.axhline(
-        y=peak,
-        label="P ≤ π",
-        linewidth=1,
-        color=next(ax._get_lines.prop_cycler)["color"],
-    )
-
-mpl.rcParams["axes.prop_cycle"] = mpl.cycler(
-    color=COLOR_LIST
-)  # sets colors using given cycle
-
-perf_max = 0.0
-
-for data_file, label in data_label_list:
-    performance_data = list()
-    with open(data_file) as f:
-        reader = csv.reader(f, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
-        n_list = reader.__next__()
-        runs_list = reader.__next__()
-        cycles_list = reader.__next__()
-
-    # compute performance (flops = n^3)
-    perf_list = list()
-    for (n, c, r) in zip(n_list, cycles_list, runs_list):
-        perf_list.append((2 * n * n * n) / c)
-
-    plt.plot(n_list, perf_list, label=label, marker="^")
-
-    perf_max = max(perf_list + [perf_max])
-
-plt.ylim(0, max(perf_max + 0.1 * perf_max, peak + 0.1 * peak))
-
-plt.grid(True, which="major", axis="y")
-
-plt.legend()
-
-plt.title(title)
-plt.xlabel("n")
-plt.semilogx(base=2)
-plt.ylabel("[flops/cycle]")
-
-data_file_name = os.path.basename(data_file).replace(".csv", "")
-outfile = "{}/{}_perf.png".format(plots_dir, data_file_name)
-plt.savefig(outfile)
-plt.clf()
-
-print("stored performance plot to {}".format(outfile))
