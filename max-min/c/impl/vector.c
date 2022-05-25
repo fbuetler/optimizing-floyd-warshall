3dcbc1e8e97789ac6abccc221598a665dfde93ac
nigglir <nigglir@student.ethz.ch>
2022-05-25 11:02:59 +0200

    adjusted vectorized mm to use doubles

--- max-min/c/impl/vector.c
+++ max-min/c/impl/vector.c
@@ -1,41 +1,41 @@
 #include "mm.h"
 #include <immintrin.h>
 
-const int floats_per_vec = 256 / 32;
+const int doubles_per_vec = 256 / 64;
 
-int floydWarshall(float *C, int N) {
+int floydWarshall(double *C, int N) {
   // compute mask for the last vector of each loop
-  __m256i mask_indices = _mm256_set_epi32(7, 6, 5, 4, 3, 2, 1, 0);
-  __m256i mask_limit = _mm256_set1_epi32(N % floats_per_vec);
-  __m256i mask = _mm256_cmpgt_epi32(mask_limit, mask_indices);
+  __m256i mask_indices = _mm256_set_epi64x(3, 2, 1, 0);
+  __m256i mask_limit = _mm256_set1_epi64x(N % doubles_per_vec);
+  __m256i mask = _mm256_cmpgt_epi64(mask_limit, mask_indices);
 
   for (int k = 0; k < N; k++) {
     for (int i = 0; i < N; i++) {
-      __m256 cik = _mm256_broadcast_ss(&C[i * N + k]);
+      __m256d cik = _mm256_broadcast_sd(&C[i * N + k]);
       int j = 0;
-      for (; j < N - (floats_per_vec - 1); j += floats_per_vec) {
+      for (; j < N - (doubles_per_vec - 1); j += doubles_per_vec) {
         // load
-        __m256 cij = _mm256_load_ps(&C[i * N + j]);
-        __m256 ckj = _mm256_load_ps(&C[k * N + j]);
+        __m256d cij = _mm256_load_pd(&C[i * N + j]);
+        __m256d ckj = _mm256_load_pd(&C[k * N + j]);
 
         // compute
-        __m256 min = _mm256_min_ps(cik, ckj);
-        __m256 res = _mm256_max_ps(cij, min);
+        __m256d min = _mm256_min_pd(cik, ckj);
+        __m256d res = _mm256_max_pd(cij, min);
 
         // store
-        _mm256_storeu_ps(&C[i * N + j], res);
+        _mm256_storeu_pd(&C[i * N + j], res);
       }
 
       // load
-      __m256 cij = _mm256_maskload_ps(&C[i * N + j], mask);
-      __m256 ckj = _mm256_maskload_ps(&C[k * N + j], mask);
+      __m256d cij = _mm256_maskload_pd(&C[i * N + j], mask);
+      __m256d ckj = _mm256_maskload_pd(&C[k * N + j], mask);
 
       // compute
-      __m256 min = _mm256_min_ps(cik, ckj);
-      __m256 res = _mm256_max_ps(cij, min);
+      __m256d min = _mm256_min_pd(cik, ckj);
+      __m256d res = _mm256_max_pd(cij, min);
 
       // store
-      _mm256_maskstore_ps(&C[i * N + j], mask, res);
+      _mm256_maskstore_pd(&C[i * N + j], mask, res);
     }
   }
   return 0;
