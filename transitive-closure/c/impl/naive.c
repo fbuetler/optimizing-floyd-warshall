d381588bbe3309c441d6c8e17b835f481e1d5659
Florian Buetler <flbuetle@ethz.ch>
2022-05-25 11:13:00 +0200

    simple c optimization in tc-naive

--- transitive-closure/c/impl/naive.c
+++ transitive-closure/c/impl/naive.c
@@ -1,5 +1,6 @@
-#include "tc.h"
 #include <math.h>
+
+#include "tc.h"
 
 /* unoptimized version of the modified Floyd-Warshall transitive-closure algorithm
  * the provided matrix is in packed-boolean form, meaning value [x, y] is only one bit long
@@ -8,23 +9,18 @@
  *   so if n % 8 != 0, there will be some unused bits at the end of each line, but in total, at most
  *   N * (7 / 8) bytes will be wasted.
  */
-int floydWarshall(char *C, int N)
-{
-  int bpl = ceil(N / 8.0); // bytes per matrix line
-  for (int k = 0; k < N; k++)
-  {
-    for (int i = 0; i < N; i++)
-    {
-      // extract C[i, k]
-      char cikb = C[i * bpl + k / 8];
-      int b_index = k % 8;
-      char mask = 1 << b_index;
-      char cik = cikb & mask ? 0xff : 0x00; // if this is 0, we can skip the innermost loop
-      for (int j = 0; j < bpl; j++)
-      {
-        C[i * bpl + j] = C[i * bpl + j] | (cik & C[k * bpl + j]);
-      }
+int floydWarshall(char *C, int N) {
+    int bpl = ceil(N / 8.0);  // bytes per matrix line
+    for (int k = 0; k < N; k++) {
+        int b_index = k % 8;
+        char mask = 1 << b_index;
+        for (int i = 0; i < N; i++) {
+            // extract C[i, k]
+            char cikb = C[i * bpl + k / 8];
+            char cik = cikb & mask ? 0xff : 0x00;  // if this is 0, we can skip the innermost loop
+            for (int j = 0; j < bpl; j++) {
+                C[i * bpl + j] = C[i * bpl + j] | (cik & C[k * bpl + j]);
+            }
+        }
+        return 0;
     }
-  }
-  return 0;
-}
