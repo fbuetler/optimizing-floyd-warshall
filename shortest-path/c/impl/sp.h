da906a228fa9abcb8ad2009fbedb27bd1d010540
Florian Buetler <flbuetle@ethz.ch>
2022-05-25 15:02:23 +0200

    align code templates to have the same structure

--- shortest-path/c/impl/sp.h
+++ shortest-path/c/impl/sp.h
@@ -2,7 +2,7 @@
 #define FW_HEADER
 
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
-#define MAX(X, Y) (((X) < (Y)) ? (Y) : (X))
+#define ADD(X, Y) ((X + Y))
 
 /* Computes the shortest paths between any pair of vertices and stores their lengths in C. N is the number of nodes.*/
 int floydWarshall(double *C, int N);
--- transitive-closure/c/impl/tc.h
+++ transitive-closure/c/impl/tc.h
@@ -3,6 +3,9 @@
 
 #include <math.h>
 
+#define AND(X, Y) ((X & Y))
+#define OR(X, Y) ((X | Y))
+
 /* Computes the transitive closure of and stores it in C. N is the number of nodes.*/
 int floydWarshall(char *C, int N);
 
