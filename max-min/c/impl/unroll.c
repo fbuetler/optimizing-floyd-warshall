66a0edde476a8a088720354c2761d0eaa1821787
nigglir <nigglir@student.ethz.ch>
2022-05-27 15:43:47 +0200

    reordered operations for max-min

--- max-min/c/impl/unroll.c
+++ max-min/c/impl/unroll.c
@@ -1,8 +1,8 @@
 #include "mm.h"
 
 /*
-unoptimized version of the modified Floyd-Warshall maximin algorithm
-*/
+ * manually unrolled version of the modified Floyd-Warshall maximin algorithm
+ */
 int floydWarshall(double *C, int N)
 {
     for (int k = 0; k < N; k++)
@@ -40,23 +40,41 @@ int floydWarshall(double *C, int N)
                 double ckj2 = C[k * N + j + 2];
                 double ckj3 = C[k * N + j + 3];
 
-                // compute
-                double res0_0 = MAX(ci0j0, MIN(ci0k, ckj0));
-                double res0_1 = MAX(ci0j1, MIN(ci0k, ckj1));
-                double res0_2 = MAX(ci0j2, MIN(ci0k, ckj2));
-                double res0_3 = MAX(ci0j3, MIN(ci0k, ckj3));
-                double res1_0 = MAX(ci1j0, MIN(ci1k, ckj0));
-                double res1_1 = MAX(ci1j1, MIN(ci1k, ckj1));
-                double res1_2 = MAX(ci1j2, MIN(ci1k, ckj2));
-                double res1_3 = MAX(ci1j3, MIN(ci1k, ckj3));
-                double res2_0 = MAX(ci2j0, MIN(ci2k, ckj0));
-                double res2_1 = MAX(ci2j1, MIN(ci2k, ckj1));
-                double res2_2 = MAX(ci2j2, MIN(ci2k, ckj2));
-                double res2_3 = MAX(ci2j3, MIN(ci2k, ckj3));
-                double res3_0 = MAX(ci3j0, MIN(ci3k, ckj0));
-                double res3_1 = MAX(ci3j1, MIN(ci3k, ckj1));
-                double res3_2 = MAX(ci3j2, MIN(ci3k, ckj2));
-                double res3_3 = MAX(ci3j3, MIN(ci3k, ckj3));
+                // compute 1
+                double min0_0 = MIN(ci0k, ckj0);
+                double min0_1 = MIN(ci0k, ckj1);
+                double min0_2 = MIN(ci0k, ckj2);
+                double min0_3 = MIN(ci0k, ckj3);
+                double min1_0 = MIN(ci1k, ckj0);
+                double min1_1 = MIN(ci1k, ckj1);
+                double min1_2 = MIN(ci1k, ckj2);
+                double min1_3 = MIN(ci1k, ckj3);
+                double min2_0 = MIN(ci2k, ckj0);
+                double min2_1 = MIN(ci2k, ckj1);
+                double min2_2 = MIN(ci2k, ckj2);
+                double min2_3 = MIN(ci2k, ckj3);
+                double min3_0 = MIN(ci3k, ckj0);
+                double min3_1 = MIN(ci3k, ckj1);
+                double min3_2 = MIN(ci3k, ckj2);
+                double min3_3 = MIN(ci3k, ckj3);
+
+                // compute 2
+                double res0_0 = MAX(ci0j0, min0_0);
+                double res0_1 = MAX(ci0j1, min0_1);
+                double res0_2 = MAX(ci0j2, min0_2);
+                double res0_3 = MAX(ci0j3, min0_3);
+                double res1_0 = MAX(ci1j0, min1_0);
+                double res1_1 = MAX(ci1j1, min1_1);
+                double res1_2 = MAX(ci1j2, min1_2);
+                double res1_3 = MAX(ci1j3, min1_3);
+                double res2_0 = MAX(ci2j0, min2_0);
+                double res2_1 = MAX(ci2j1, min2_1);
+                double res2_2 = MAX(ci2j2, min2_2);
+                double res2_3 = MAX(ci2j3, min2_3);
+                double res3_0 = MAX(ci3j0, min3_0);
+                double res3_1 = MAX(ci3j1, min3_1);
+                double res3_2 = MAX(ci3j2, min3_2);
+                double res3_3 = MAX(ci3j3, min3_3);
 
                 // store
                 C[(i + 0) * N + j + 0] = res0_0;
@@ -103,11 +121,17 @@ int floydWarshall(double *C, int N)
                 double ckj2 = C[k * N + j + 2];
                 double ckj3 = C[k * N + j + 3];
 
-                // compute
-                double res0 = MAX(cij0, MIN(cik, ckj0));
-                double res1 = MAX(cij1, MIN(cik, ckj1));
-                double res2 = MAX(cij2, MIN(cik, ckj2));
-                double res3 = MAX(cij3, MIN(cik, ckj3));
+                // compute 1
+                double min0 = MIN(cik, ckj0);
+                double min1 = MIN(cik, ckj1);
+                double min2 = MIN(cik, ckj2);
+                double min3 = MIN(cik, ckj3);
+
+                // compute 2
+                double res0 = MAX(cij0, min0);
+                double res1 = MAX(cij1, min1);
+                double res2 = MAX(cij2, min2);
+                double res3 = MAX(cij3, min3);
 
                 // store
                 C[i * N + j + 0] = res0;
