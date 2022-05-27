1b6bf6754a7c244515b7cc351442e935d304e89f
lmeinen <lmeinen@student.ethz.ch>
2022-05-27 17:11:06 +0200

    Wrote cache simulator to compute cache misses (SLOW)

---
+++ measurements/simulate_cache.py
@@ -0,0 +1,130 @@
+import argparse
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+cache = {}
+
+parser = argparse.ArgumentParser(description='analyzes the caching behavior of a program')
+parser.add_argument(
+    "-size", "--cache-size", help="cache size in bytes", type=int, required=True
+)
+parser.add_argument(
+    "-e", "--cache-assoc", help="cache associativity", type=int, required=True
+)
+parser.add_argument(
+    "-d",
+    "--data-size",
+    help="size in bytes of a single matrix element",
+    type=int,
+    required=True
+)
+parser.add_argument("-n", "--input-size", help="input size", type=int, required=True)
+
+
+def init_cache(S: int, E: int):
+    """ Assumes 64 byte blocks
+        - S is the number of cache sets
+        - E is the cache associativity
+    """
+    cache["S"] = S
+    cache["E"] = E
+    cache['hits'] = 0
+    cache['misses'] = 0
+    for s in range(S):
+        cache[s] = {}
+        for e in range(E):
+            cache[s][e] = {}
+            cache[s][e]['tag'] = -1
+            cache[s][e]['t'] = -1
+
+def reset_cache():
+    cache['hits'] = 0
+    cache['misses'] = 0
+    for s in range(cache['S']):
+        cache[s] = {}
+        for e in range(cache['E']):
+            cache[s][e] = {}
+            cache[s][e]['tag'] = -1
+            cache[s][e]['t'] = -1
+
+def access(i: int, j: int, n:int, data_size: int, t: int, operation: str) -> int:
+    # compute tag and set identifiers
+    # HACK: Assume cache line size to be 64 bytes and divisible by sizeof(datatype)
+    tag = (i * n + j) // (64//data_size)
+    set = tag % cache["S"]
+
+    # simulate cache access
+    hit = False
+    for e in range(cache["E"]):
+        if cache[set][e]['tag'] == tag:
+            hit = True
+            cache['hits'] += 1
+            cache[set][e]['t'] = t
+            break
+    if not hit:
+        cache['misses'] += 1
+
+        # find LRU and replace
+        min_e = 0
+        for e in range(1, cache["E"]):
+            if cache[set][e]['t'] < cache[set][min_e]['t']:
+                min_e = e
+        cache[set][min_e]['tag'] = tag
+        cache[set][min_e]['t'] = t
+
+    # log event
+    hm = 'H' if hit else 'M'
+    logging.debug(f"{operation} to ({i}, {j}): {hm} in line {set}")
+
+    # return
+    return t+1
+
+
+def output_cache(n:int, data_size: int):
+    # output number of hits and misses
+    logging.info(f"#hits: {cache['hits']}  #misses: {cache['misses']}")
+    cache['hits'] = 0
+    cache['misses'] = 0
+
+    # output cache contents if logging level set to DEBUG
+    if logging.getLogger().isEnabledFor(logging.DEBUG):
+        elems_per_line = 64//data_size
+
+        for s in range(cache['S']):
+            for e in range(cache['E']):
+                tag = cache[s][e]['tag']
+                if tag == -1:
+                    field1 = '-'
+                    field2 = '-'
+                    state = ' '.join('-' for _ in range(elems_per_line))
+                    logging.debug(f'line{s}: {state}')
+                else:
+                    def indices(ind):
+                        j = ind % n
+                        i = (ind - j) // n
+                        return (i, j) if i < n and j < n else ('-', '-')
+                    state = ' '.join(f'C{i}{j}' for (i,j) in [indices(tag * elems_per_line + offset) for offset in range(elems_per_line)])
+                    logging.debug(f'line {s}, set {e}: {state}')
+
+def naive(n: int, data_size: int):
+    # HACK: Caching behavior for naive, unrolled and vector implementations should be the same
+    t = 0
+    for k in range(n):
+        logging.info(f'k is at {k}')
+        for i in range(n):
+            for j in range(n):
+                t = access(i, k, n, data_size, t, 'R')
+                t = access(k, j, n, data_size, t, 'R')
+                t = access(i, j, n, data_size, t, 'W')
+    logging.info('outputting cache...')
+    output_cache(n, data_size)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    e = args.cache_assoc
+    s = args.cache_size // (e * 64)
+    logging.info(f'initializing cache with params (S,E,B) = ({s},{e},64)...')
+    init_cache(s,e)
+    naive(args.input_size, args.data_size)
+    reset_cache()
